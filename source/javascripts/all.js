fb3f506de86b3b9ef36a3d993fde01c4d08aa6da
Buddy Magsipoc <keikun17@gmail.com>
2013-08-22 16:56:49 +0800

    Reactor Online, Sensors Online, Weapons Online, All Systems : Nominal.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,42 @@
 //= require_tree .
+
+$(function(){
+  console.log("Reactor: Online")
+  // get heat value
+  currentHeat = function(){
+    return parseInt($('.progress .progress-bar').attr('aria-valuetransitiongoal'));
+  }
+
+  // Reset upon completion
+  coolDown = function(){
+    towards = currentHeat() - 1;
+
+    $('.progress .progress-bar').attr('aria-valuetransitiongoal', towards);
+    $('.progress .progress-bar').progressbar({
+      refresh_rate: 10,
+      display_text: 'fill'
+    })
+
+  };
+
+  // Run heatsinks
+  runHeatsink = function(){
+    console.log("Sensors: Online")
+    setInterval(coolDown,100);
+  };
+  runHeatsink();
+
+  // Shoot at start
+  shoot = function(val){
+    console.log('Weapons: Online');
+
+    $('.progress .progress-bar').attr('aria-valuetransitiongoal', val);
+    $('.progress .progress-bar').progressbar({
+      display_text: 'fill'
+    });
+
+  };
+
+  console.log("All Systems: Nominal")
+  shoot(55);
+});
