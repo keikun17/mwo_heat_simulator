e23c119fde76f8ccb41402ef3b69c17eb5ddd0a0
Buddy Magsipoc <keikun17@gmail.com>
2013-08-23 20:23:08 +0800

    Heatsinks.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,38 +4,48 @@ $(function(){
   console.log("Reactor: Online")
   // get heat value
   currentHeat = function(){
-    return parseInt($('#heatlevel').attr('aria-valuetransitiongoal'));
+    val = parseInt($('#heatlevel').attr('aria-valuetransitiongoal'));
+    if( isNaN(val) ) {
+      val = 0;
+    }
+    return val;
   }
 
-  // Reset upon completion
-  coolDown = function(){
-    towards = currentHeat() - 1;
-
-    $('#heatlevel').attr('aria-valuetransitiongoal', towards);
-    $('#heatlevel').progressbar({
-      refresh_rate: 10,
-      display_text: 'fill'
-    })
-
-  };
+  heatSinkCount = function(){
+    return parseInt($('#heatsink-count').val());
+  }
 
   // Run heatsinks
+  coolDown = function(){
+    towards = currentHeat() - (.1 * heatSinkCount() );
+
+    if( currentHeat() > 0 ) {
+      $('#heatlevel').attr('aria-valuetransitiongoal', towards);
+      $('#heatlevel').progressbar({
+        refresh_speed: 10,
+        display_text: 'fill'
+      })
+    }
+  };
+
+  coolDownRate = function(){
+    return 1000;
+  }
+
   runHeatsink = function(){
     console.log("Sensors: Online")
-    setInterval(coolDown,100);
+    setInterval(coolDown, coolDownRate());
   };
 
   runHeatsink();
 
   // Shoot at start
   shoot = function(val){
-    console.log('Weapons: Online');
-
-    $('#heatlevel').attr('aria-valuetransitiongoal', val);
+    towards = val + currentHeat();
+    $('#heatlevel').attr('aria-valuetransitiongoal', towards);
     $('#heatlevel').progressbar({
       display_text: 'fill'
     });
-
   };
   console.log('Weapons: Online');
 
