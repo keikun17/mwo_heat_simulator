42778c1bd500093fb8e361ea6f41321797c59dab
Buddy Magsipoc <keikun17@gmail.com>
2013-08-27 22:02:06 +0800

    Buttons.

--- source/javascripts/templates.js
+++ source/javascripts/templates.js
@@ -1,5 +1,5 @@
 $(function(){
-  weaponView = _.template("<li> <%= name %> <a href='#' class='js-fire' data-weapon-class='<%= weaponClass %>'> Fire</a> <a href='#' class='js-strip'>Strip</a> </li>");
+  weaponView = _.template("<li> <%= name %> <a href='#' class='js-fire btn-xs btn-danger' data-weapon-class='<%= weaponClass %>'> Fire</a> <a href='#' class='js-strip btn-xs btn-warning'>Strip</a> </li>");
 })
 
 
