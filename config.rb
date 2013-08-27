dedf3c8e3241062956191001cfbce6551c952d58
Buddy Magsipoc <keikun17@gmail.com>
2013-08-28 00:19:12 +0800

    Use Sprockets FFS.

--- config.rb
+++ config.rb
@@ -68,3 +68,5 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+sprockets.append_path File.join "#{root}", 'source/components'
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,8 @@
-//= require_tree .
+//= require 'jquery/jquery.js'
+//= require 'bootstrap/dist/js/bootstrap.js'
+//= require 'underscore/underscore.js'
+//= require 'bootstrap-progressbar/bootstrap-progressbar.js'
+//= require_tree
 
 $(function(){
   shoot(5);
