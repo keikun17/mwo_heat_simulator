327754a7489c5e993135aa3ea5cf61f344d83f13
Buddy Magsipoc <keikun17@gmail.com>
2013-08-28 00:28:00 +0800

    move activate call inside 'config' block.

--- config.rb
+++ config.rb
@@ -63,13 +63,12 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
 
 sprockets.append_path File.join "#{root}", 'source/components'
-activate :relative_assets
 set :relative_links, true
 
