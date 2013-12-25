24ed66465d45f3b4b4c61c8b4802e2132e1cba8d
Buddy Magsipoc <keikun17@gmail.com>
2013-12-25 09:31:49 +0800

    Site updated to 204564a

--- javascripts/mech.js
+++ javascripts/mech.js
@@ -509,7 +509,23 @@
       changemap: function() {
         window.mech.refit();
         $('#dissipation').text("" + (window.map.modifier.dissipation() * 100) + "%");
-        return $('#capacity').text("" + (window.map.modifier.capacity() * 100) + "%");
+        $('#dissipation').removeClass();
+        if (window.map.modifier.dissipation() === 1) {
+          $('#dissipation').addClass('label label-info');
+        } else if (window.map.modifier.dissipation() > 1) {
+          $('#dissipation').addClass('label label-success');
+        } else if (window.map.modifier.dissipation() < 1) {
+          $('#dissipation').addClass('label label-danger');
+        }
+        $('#capacity').text("" + (window.map.modifier.capacity() * 100) + "%");
+        $('#capacity').removeClass();
+        if (window.map.modifier.capacity() === 1) {
+          return $('#capacity').addClass('label label-info');
+        } else if (window.map.modifier.capacity() > 1) {
+          return $('#capacity').addClass('label label-success');
+        } else if (window.map.modifier.capacity() < 1) {
+          return $('#capacity').addClass('label label-danger');
+        }
       },
       modifier: {
         capacity: function() {
