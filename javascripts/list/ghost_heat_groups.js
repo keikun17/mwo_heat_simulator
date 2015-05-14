da2304e9b45cbdf6b38a9bf89416927cbc6bd04c
Buddy Magsipoc <keikun17@gmail.com>
2015-05-14 12:34:55 +0800

    Site updated to d3507db

--- javascripts/list/ghost_heat_groups.js
+++ javascripts/list/ghost_heat_groups.js
@@ -1,44 +1,44 @@
 (function() {
   window.ghostHeatGroups = {
-    2: {
-      weapon_ids: ["1002", "1027", "1028", "1042", "1043", "1044"],
-      ghost_heat_trigger: "3"
+    '2': {
+      'weapon_ids': ['1002', '1026', '1027', '1028', '1041', '1042', '1043', '1044'],
+      'ghost_heat_trigger': '3'
     },
-    4: {
-      weapon_ids: ["1004", "1031", "1039", "1040"],
-      ghost_heat_trigger: "4"
+    '4': {
+      'weapon_ids': ['1004', '1031', '1039', '1040'],
+      'ghost_heat_trigger': '4'
     },
-    3: {
-      weapon_ids: ["1005", "1008", "1010", "1213", "1216"],
-      ghost_heat_trigger: "3"
+    '3': {
+      'weapon_ids': ['1005', '1008', '1010', '1213', '1216'],
+      'ghost_heat_trigger': '4'
     },
-    1: {
-      weapon_ids: ["1006", "1009", "1217"],
-      ghost_heat_trigger: "3"
+    '1': {
+      'weapon_ids': ['1006', '1009', '1217'],
+      'ghost_heat_trigger': '3'
     },
-    8: {
-      weapon_ids: ["1206", "1240"],
-      ghost_heat_trigger: "3"
+    '8': {
+      'weapon_ids': ['1206', '1240'],
+      'ghost_heat_trigger': '3'
     },
-    9: {
-      weapon_ids: ["1207", "1241"],
-      ghost_heat_trigger: "2"
+    '9': {
+      'weapon_ids': ['1207', '1241'],
+      'ghost_heat_trigger': '2'
     },
-    10: {
-      weapon_ids: ["1211", "1212", "1214", "1215"],
-      ghost_heat_trigger: "7"
+    '10': {
+      'weapon_ids': ['1211', '1212', '1214', '1215'],
+      'ghost_heat_trigger': '7'
     },
-    6: {
-      weapon_ids: ["1219", "1220", "1221", "1223", "1224", "1225"],
-      ghost_heat_trigger: "3"
+    '6': {
+      'weapon_ids': ['1218', '1219', '1220', '1221', '1222', '1223', '1224', '1225'],
+      'ghost_heat_trigger': '4'
     },
-    7: {
-      weapon_ids: ["1227", "1228", "1230", "1231"],
-      ghost_heat_trigger: "4"
+    '7': {
+      'weapon_ids': ['1227', '1228', '1230', '1231'],
+      'ghost_heat_trigger': '4'
     },
-    5: {
-      weapon_ids: ["1233", "1234"],
-      ghost_heat_trigger: "4"
+    '5': {
+      'weapon_ids': ['1233', '1234'],
+      'ghost_heat_trigger': '4'
     }
   };
 
